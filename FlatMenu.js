206de95da3bcb061c116a5e530cfd1e5481f4540
zakuni <kunio038@gmail.com>
2011-02-21 23:21:55 +0900

    メニュー見出しをh3に変えた。
    項目追加したり、A-Zの部分を足したりした。
    メニューのソートもするように(英語しかできない)。

--- FlatMenu.js
+++ FlatMenu.js
@@ -5,28 +5,42 @@ $(function(){
     var scroll_now = 0;
 
     var data = [
-	{"item": "論文", "yomi":"ra", "link": "http://masui.sfc.keio.ac.jp"},
-	{"item": "説明", "yomi":"sa", "link": "http://masui.sfc.keio.ac.jp"}
+	{"item": "SnapScroll", "yomi":"a-z", "link": "http://pitecan.com"},
+	{"item": "FlatMenu", "yomi":"a-z", "link": "http://pitecan.com"},
+	{"item": "About", "yomi":"a-z", "link": "http://pitecan.com"},
+	{"item": "論文", "yomi":"ra", "link": "http://masui.sfc.keio.ac.jp/pub/zakuni_interaction2011_paper.pdf"},
+	{"item": "説明", "yomi":"sa", "link": "http://masui.sfc.keio.ac.jp"},
+	{"item": "ソース", "yomi":"sa", "link": "https://github.com/zakuni/FlatMenu"},
+	{"item": "さがす", "yomi":"sa", "link": "https://github.com/zakuni/FlatMenu"}
     ]
 
+    function SortByName(x,y) {
+      return ((x.item == y.item) ? 0 : ((x.item > y.item) ? 1 : -1 ));
+    }
+
+    // Call Sort By Name
+    data.sort(SortByName);
+
     for(var i in data){
-	$("#" + data[i].yomi).append("<br><a href='" + data[i].link + "'>" + data[i].item + "</a>");
+	$("#" + data[i].yomi).append("<li><a href='" + data[i].link + "'>" + data[i].item + "</a></li>");
 }
+
 
     var menu_position = $('div#menuu').offset().top
     // あかさたなの初期位置
-    var start = new Array(10);
+    var start = new Array(11);
 
-    start[0] = $('div#a').offset().top
-    start[1] = $('div#ka').offset().top
-    start[2] = $('div#sa').offset().top
-    start[3] = $('div#ta').offset().top
-    start[4] = $('div#na').offset().top
-    start[5] = $('div#ha').offset().top
-    start[6] = $('div#ma').offset().top
-    start[7] = $('div#ya').offset().top
-    start[8] = $('div#ra').offset().top
-    start[9] = $('div#wa').offset().top    
+    start[0] = $('h3#az').offset().top
+    start[1] = $('h3#a').offset().top
+    start[2] = $('h3#k').offset().top
+    start[3] = $('h3#s').offset().top
+    start[4] = $('h3#t').offset().top
+    start[5] = $('h3#n').offset().top
+    start[6] = $('h3#h').offset().top
+    start[7] = $('h3#m').offset().top
+    start[8] = $('h3#y').offset().top
+    start[9] = $('h3#r').offset().top
+    start[10] = $('h3#w').offset().top    
 
     var start_reverse = $($(start).get().reverse()); // startを逆順にした配列
 
@@ -55,7 +69,7 @@ $(function(){
 	    $(start_reverse).each(
 		function(i){
 		    if(scroll_now > start_reverse[i]){
-			menuu.scrollTop(start_reverse[i+1] - menu_position);
+			menuu.scrollTop(start_reverse[i] - menu_position);
 			return false;
 		    }
 		}
