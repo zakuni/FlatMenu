973a2ef585d70a383a49e234aa2e58e8e5794f30
Kunio Ozawa <kunio038@gmail.com>
2012-09-09 02:40:07 -0400

    インデントの修正

--- FlatMenu.js
+++ FlatMenu.js
@@ -10,33 +10,33 @@ $(function(){
     }
 
 
-    var menu_position
+    var menu_position;
     var start = new Array(11);
-    var start_reverse
+    var start_reverse;
     
    $.getJSON('data.json', function(data){
 	// Call Sort By Name
 	data.sort(SortByName);
 
 	for(var i in data){ // 項目をメニューに挿入
-	    $("#" + data[i].id + " .left").append("<a href='" + data[i].link + "'>" + data[i].left + "</a><br>");
-	    $("#" + data[i].id + " .right").append("<a href='" + data[i].link + "'>" +  data[i].right +"<br>");
+        $("#" + data[i].id + " .left").append("<a href='" + data[i].link + "'>" + data[i].left + "</a><br>");
+        $("#" + data[i].id + " .right").append("<a href='" + data[i].link + "'>" +  data[i].right +"<br>");
 	}
 
-	menu_position = $('div#menuu').offset().top
+	menu_position = $('div#menuu').offset().top;
 	// あかさたなの初期位置
 
-	start[0] = $('div#az').offset().top
-	start[1] = $('div#a').offset().top
-	start[2] = $('div#k').offset().top
-	start[3] = $('div#s').offset().top
-	start[4] = $('div#t').offset().top
-	start[5] = $('div#n').offset().top
-	start[6] = $('div#h').offset().top
-	start[7] = $('div#m').offset().top
-	start[8] = $('div#y').offset().top
-	start[9] = $('div#r').offset().top
-	start[10] = $('div#w').offset().top    
+	start[0] = $('div#az').offset().top;
+	start[1] = $('div#a').offset().top;
+	start[2] = $('div#k').offset().top;
+	start[3] = $('div#s').offset().top;
+	start[4] = $('div#t').offset().top;
+	start[5] = $('div#n').offset().top;
+	start[6] = $('div#h').offset().top;
+	start[7] = $('div#m').offset().top;
+	start[8] = $('div#y').offset().top;
+	start[9] = $('div#r').offset().top;
+	start[10] = $('div#w').offset().top;    
 
 	start_reverse = $($(start).get().reverse()); // startを逆順にした配列
     });
@@ -46,30 +46,30 @@ $(function(){
     });
 
     barr.scroll(function(){
-	scroll_now = barr.scrollTop()  // 現在のスクロール位置
-	scroll_amount = scroll_now - scroll_start // スクロール量
+	scroll_now = barr.scrollTop();  // 現在のスクロール位置
+	scroll_amount = scroll_now - scroll_start; // スクロール量
 
 	// 一つ後のあかさたな要素までスナップ
 	if(scroll_amount > 200){
-	    $(start).each(
+        $(start).each(
 		function(i){
-		    if(scroll_now < start[i]){
+            if(scroll_now < start[i]){
 			menuu.scrollTop(start[i+1] - menu_position);
 			return false; // breakの代わりにこう書く
-		    }
+            }
 		}
-	    );
+        );
 	}
 	// 一つ前のあかさたな要素までスナップ
 	else if(scroll_amount < -200){
-	    $(start_reverse).each(
+        $(start_reverse).each(
 		function(i){
-		    if(scroll_now > start_reverse[i]){
+            if(scroll_now > start_reverse[i]){
 			menuu.scrollTop(start_reverse[i] - menu_position);
 			return false;
-		    }
+            }
 		}
-	    );
+        );
 	}
 	// 普通にスクロール
 	else if(scroll_amount != 0){
